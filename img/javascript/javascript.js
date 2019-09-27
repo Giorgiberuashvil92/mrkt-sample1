50ecc5a06e1e6b43acb8c789a9f95e4cb5e97c8c
Giorgi <gberuashvili92@gmail.com>
2019-09-27 17:53:31 +0400

    initial commit

---
+++ img/javascript/javascript.js
@@ -0,0 +1,361 @@
+const arr = [{
+        id: "1",
+        name: 'Matériel Tbilisi',
+        name2: 'Nevy denim slim fit jeans',
+        price: 250,
+        newest: true,
+        img: "https://i.ibb.co/crn4KwM/first.png",
+    },
+    {
+        id: 2,
+        name: 'Matériel Tbilisi',
+        newest: true,
+        name2: 'Nevy denim slim fit jeans',
+        price: 260,
+        img: "https://i.ibb.co/b5xW8yw/second.png"
+
+    },
+    {
+        id: 2,
+        name: 'Matériel Tbilisi',
+        newest: false,
+        name2: 'Nevy denim slim fit jeans',
+        price: 280,
+        img: "https://i.ibb.co/vxK0Dq0/four.png"
+    },
+    {
+        id: 2,
+        name: 'Matériel Tbilisi',
+        newest: true,
+        name2: 'Nevy denim slim fit jeans',
+        price: 330,
+        img: "https://i.ibb.co/G76yssL/third.png",
+    }, {
+        id: 2,
+        name: 'Matériel Tbilisi',
+        newest: false,
+        name2: 'Nevy denim slim fit jeans',
+        price: 350,
+        img: "https://i.ibb.co/G76yssL/third.png",
+
+    }, {
+        id: 5,
+        name: 'Matériel Tbilisi',
+        newest: false,
+        name2: 'Nevy denim slim fit jeans',
+        price: 150,
+        img: "https://i.ibb.co/zhgZpL2/pink.png"
+
+
+    }, {
+        id: 7,
+        name: 'Matériel Tbilisi',
+        newest: false,
+        name2: 'Nevy denim slim fit jeans',
+        price: 550,
+        img: "https://i.ibb.co/G76yssL/third.png",
+
+
+    }, {
+        id: 8,
+        name: 'Matériel Tbilisi',
+        newest: false,
+        name2: 'Nevy denim slim fit jeans',
+        price: 650,
+        img: "https://i.ibb.co/G76yssL/third.png",
+
+
+    }, {
+        id: 6,
+        name: 'Matériel Tbilisi',
+        newest: false,
+        name2: 'Nevy denim slim fit jeans',
+        price: 750,
+        img: "https://i.ibb.co/G76yssL/third.png",
+
+    }, {
+        id: 10,
+        name: 'Matériel Tbilisi',
+        newest: false,
+        name2: 'Nevy denim slim fit jeans',
+        price: 700,
+        img: "https://i.ibb.co/G76yssL/third.png",
+
+    }, {
+        id: 11,
+        name: 'Matériel Tbilisi',
+        newest: false,
+        name2: 'Nevy denim slim fit jeans',
+        price: 750,
+        img: "https://i.ibb.co/G76yssL/third.png",
+
+    }, {
+        id: 5,
+        name: 'Matériel Tbilisi',
+        newest: false,
+        name2: 'Nevy denim slim fit jeans',
+        price: 550,
+        img: "https://i.ibb.co/G76yssL/third.png",
+
+    }, {
+        id: 13,
+        name: 'Matériel Tbilisi',
+        newest: false,
+        name2: 'Nevy denim slim fit jeans',
+        price: 750,
+        img: "https://i.ibb.co/G76yssL/third.png",
+
+    }, {
+        id: 14,
+        name: 'Matériel Tbilisi',
+        newest: false,
+        name2: 'Nevy denim slim fit jeans',
+        price: 850,
+        img: "https://i.ibb.co/G76yssL/third.png",
+
+    }, {
+        id: 15,
+        name: 'Matériel Tbilisi',
+        newest: false,
+        name2: 'Nevy denim slim fit jeans',
+        price: 850,
+        img: "https://i.ibb.co/G76yssL/third.png",
+
+    }, {
+        id: 16,
+        name: 'Matériel Tbilisi',
+        newest: false,
+        name2: 'Nevy denim slim fit jeans',
+        price: 850,
+        img: "https://i.ibb.co/G76yssL/third.png",
+
+    }, {
+        id: 17,
+        name: 'Matériel Tbilisi',
+        newest: false,
+        name2: 'Nevy denim slim fit jeans',
+        price: 850,
+        img: "https://i.ibb.co/G76yssL/third.png",
+
+    }, {
+        id: 18,
+        name: 'Matériel Tbilisi',
+        newest: false,
+        name2: 'Nevy denim slim fit jeans',
+        price: 850,
+        img: "https://i.ibb.co/G76yssL/third.png",
+
+    }, {
+        id: 19,
+        name: 'Matériel Tbilisi',
+        newest: false,
+        name2: 'Nevy denim slim fit jeans',
+        price: 850,
+        img: "https://i.ibb.co/G76yssL/third.png",
+
+    }, {
+        id: 20,
+        name: 'Matériel Tbilisi',
+        newest: false,
+        name2: 'Nevy denim slim fit jeans',
+        price: 850,
+        img: "https://i.ibb.co/G76yssL/third.png",
+
+    }, {
+        id: 21,
+        name: 'Matériel Tbilisi',
+        newest: false,
+        name2: 'Nevy denim slim fit jeans',
+        price: 850,
+        img: "https://i.ibb.co/G76yssL/third.png",
+
+    }, {
+        id: 22,
+        name: 'Matériel Tbilisi',
+        newest: false,
+        name2: 'Nevy denim slim fit jeans',
+        price: 850,
+        img: "https://i.ibb.co/G76yssL/third.png",
+
+    }, {
+        id: 25,
+        name: 'Matériel Tbilisi',
+        newest: false,
+        name2: 'Nevy denim slim fit jeans',
+        price: 850,
+        img: "https://i.ibb.co/G76yssL/third.png",
+
+    }, {
+        id: 25,
+        name: 'Matériel Tbilisi',
+        newest: false,
+        name2: 'Nevy denim slim fit jeans',
+        price: 850,
+        img: "https://i.ibb.co/b5xW8yw/second.png"
+
+    }, {
+        id: 25,
+        name: 'Matériel Tbilisi',
+        newest: false,
+        name2: 'Nevy denim slim fit jeans',
+        price: 850,
+        img: "https://i.ibb.co/b5xW8yw/second.png"
+
+    }, {
+        id: 25,
+        name: 'Matériel Tbilisi',
+        newest: false,
+        name2: 'Nevy denim slim fit jeans',
+        price: 850,
+        img: "https://i.ibb.co/b5xW8yw/second.png"
+
+    }, {
+        id: 25,
+        name: 'Matériel Tbilisi',
+        newest: false,
+        name2: 'Nevy denim slim fit jeans',
+        price: 850,
+        img: "https://i.ibb.co/G76yssL/third.png",
+
+    }, {
+        id: 24,
+        name: 'Matériel Tbilisi',
+        newest: false,
+        name2: 'Nevy denim slim fit jeans',
+        price: 850,
+        img: "https://i.ibb.co/b5xW8yw/second.png"
+
+    }
+];
+
+function testfunction() {
+    var filterbynewest = arr.filter(function(newest) {
+        return newest.newest == true;
+    });
+
+    var output = document.getElementById('output');
+    document.getElementById("productid1").style.display = "none";
+    document.getElementById("productid2").style.display = "none";
+    document.getElementById("productid3").style.display = "none";
+    document.getElementById("productid4").style.display = "none";
+    output.innerHTML = filterbynewest.map(product => {
+        return `<li data-id="${product.id}">
+                    <img src="${product.img}" >
+                     <span>${ product.name }</span><br>
+                     <span>${product.name2}</span><br>
+                     <span>${product.price} </span>
+</li>`;
+    }).join('');
+}
+console.log(testfunction);
+
+function restfunction() {
+    var filterbypicks = arr.filter(function(pickup) {
+        return pickup.id == 2;
+    });
+    var output1 = document.getElementById('output1');
+    document.getElementById("productid1").style.display = "none";
+    document.getElementById("productid2").style.display = "none";
+    document.getElementById("productid3").style.display = "none";
+    document.getElementById("productid4").style.display = "none";
+    // document.getElementById("output").style.display = "none";
+    document.getElementById("output").style.display = "none";
+
+    output1.innerHTML = filterbypicks.map(product => {
+        return `<li data-id="${product.id}">
+                    <img src="${product.img}" >
+                     <span>${ product.name }</span><br>
+                     <span>${product.name2}</span><br>
+                     <span>${product.price} </span>
+</li>`;
+    }).join('');
+}
+
+function designfunction() {
+    var designpicks = arr.filter(function(design) {
+        return design.id == 25;
+    });
+    var output2 = document.getElementById("output2");
+    document.getElementById("productid1").style.display = "none";
+    document.getElementById("productid2").style.display = "none";
+    document.getElementById("productid3").style.display = "none";
+    document.getElementById("productid4").style.display = "none";
+    document.getElementById("output1").style.display = "none";
+    document.getElementById("output").style.display = "none";
+
+    output2.innerHTML = designpicks.map(product => {
+        return `<li data-id="${product.id}">
+                 <img src="${product.img}" >
+                <span>${ product.name }</span><br>
+                <span>${product.name2}</span><br>
+                <span>${product.price} </span>
+</li>`;
+    }).join('');
+}
+
+function pricehigh() {
+    var pricehigh1 = arr.filter(function(e) {
+            return e.price <= 950;
+        })
+        .sort(function(a, b) {
+            if (a.price < b.price) return -1
+            else return 1
+        })
+    var output3 = document.getElementById("output3");
+    document.getElementById("productid1").style.display = "none";
+    document.getElementById("productid2").style.display = "none";
+    document.getElementById("productid3").style.display = "none";
+    document.getElementById("productid4").style.display = "none";
+    document.getElementById("output1").style.display = "none";
+    document.getElementById("output").style.display = "none";
+    document.getElementById("output2").style.display = "none";
+
+
+
+    output3.innerHTML = pricehigh1.map(product => {
+        return `<li data-id="${product.id}">
+                 <img src="${product.img}" >
+                <span>${ product.name }</span><br>
+                <span>${product.name2}</span><br>
+                <span>${product.price} </span>
+</li>`;
+    }).join('')
+}
+
+function price() {
+    var pricelow = arr.filter(function(g) {
+            return g.price <= 950;
+        })
+        .sort(function(a, b) {
+            if (a.price < b.price) return 1
+            else return -1
+        })
+
+
+    var output4 = document.getElementById("output4");
+    document.getElementById("productid1").style.display = "none";
+    document.getElementById("productid2").style.display = "none";
+    document.getElementById("productid3").style.display = "none";
+    document.getElementById("productid4").style.display = "none";
+
+
+    output4.innerHTML = pricelow.map(product => {
+        return `<li data-id="${product.id}">
+                 <img src="${product.img}" >
+                 <span>${ product.name }</span><br>
+                 <span>${product.name2}</span><br>
+                 <span>${product.price} </span>
+</li>`;
+    }).join('')
+}
+
+var slider = document.getElementById('slider');
+
+noUiSlider.create(slider, {
+    start: [20, 80],
+    connect: true,
+    range: {
+        'min': 0,
+        'max': 100
+    }
+});
---
+++ javascript/javascript.js
@@ -0,0 +1,347 @@
+const types = {
+    NEWEST: 'newest',
+    OUR_PICKS: 'picks'
+}
+const arr = [{
+        id: "1",
+        name: 'Matériel Tbilisi',
+        name2: 'Nevy denim slim fit jeans',
+        price: 250,
+        newest: true,
+        img: "https://i.ibb.co/crn4KwM/first.png",
+        type: types.NEWEST
+    },
+    {
+        id: 2,
+        name: 'Matériel Tbilisi',
+        newest: true,
+        name2: 'Nevy denim slim fit jeans',
+        price: 260,
+        img: "https://i.ibb.co/b5xW8yw/second.png",
+        type: types.OUR_PICKS,
+        design: 'ingorokva'
+
+    },
+    {
+        id: 2,
+        name: 'Matériel Tbilisi',
+        newest: false,
+        name2: 'Nevy denim slim fit jeans',
+        price: 280,
+        img: "https://i.ibb.co/vxK0Dq0/four.png",
+        design: 'ingorokva'
+    },
+    {
+        id: 2,
+        name: 'Matériel Tbilisi',
+        newest: true,
+        name2: 'Nevy denim slim fit jeans',
+        price: 330,
+        img: "https://i.ibb.co/G76yssL/third.png",
+        design: 'ingorokva'
+    }, {
+        id: 2,
+        name: 'Matériel Tbilisi',
+        newest: false,
+        name2: 'Nevy denim slim fit jeans',
+        price: 350,
+        img: "https://i.ibb.co/G76yssL/third.png",
+        design: 'ingorokva'
+
+    }, {
+        id: 5,
+        name: 'Matériel Tbilisi',
+        newest: false,
+        name2: 'Nevy denim slim fit jeans',
+        price: 150,
+        img: "https://i.ibb.co/zhgZpL2/pink.png"
+
+
+    }, {
+        id: 7,
+        name: 'Matériel Tbilisi',
+        newest: false,
+        name2: 'Nevy denim slim fit jeans',
+        price: 550,
+        img: "https://i.ibb.co/G76yssL/third.png",
+
+
+    }, {
+        id: 8,
+        name: 'Matériel Tbilisi',
+        newest: false,
+        name2: 'Nevy denim slim fit jeans',
+        price: 650,
+        img: "https://i.ibb.co/G76yssL/third.png",
+
+
+    }, {
+        id: 6,
+        name: 'Matériel Tbilisi',
+        newest: false,
+        name2: 'Nevy denim slim fit jeans',
+        price: 750,
+        img: "https://i.ibb.co/G76yssL/third.png",
+
+    }, {
+        id: 10,
+        name: 'Matériel Tbilisi',
+        newest: false,
+        name2: 'Nevy denim slim fit jeans',
+        price: 700,
+        img: "https://i.ibb.co/G76yssL/third.png",
+
+    }, {
+        id: 11,
+        name: 'Matériel Tbilisi',
+        newest: false,
+        name2: 'Nevy denim slim fit jeans',
+        price: 750,
+        img: "https://i.ibb.co/G76yssL/third.png",
+
+    }, {
+        id: 5,
+        name: 'Matériel Tbilisi',
+        newest: false,
+        name2: 'Nevy denim slim fit jeans',
+        price: 550,
+        img: "https://i.ibb.co/G76yssL/third.png",
+
+    }, {
+        id: 13,
+        name: 'Matériel Tbilisi',
+        newest: false,
+        name2: 'Nevy denim slim fit jeans',
+        price: 750,
+        img: "https://i.ibb.co/G76yssL/third.png",
+
+    }, {
+        id: 14,
+        name: 'Matériel Tbilisi',
+        newest: false,
+        name2: 'Nevy denim slim fit jeans',
+        price: 850,
+        img: "https://i.ibb.co/G76yssL/third.png",
+
+    }, {
+        id: 15,
+        name: 'Matériel Tbilisi',
+        newest: false,
+        name2: 'Nevy denim slim fit jeans',
+        price: 850,
+        img: "https://i.ibb.co/G76yssL/third.png",
+
+    }, {
+        id: 16,
+        name: 'Matériel Tbilisi',
+        newest: false,
+        name2: 'Nevy denim slim fit jeans',
+        price: 850,
+        img: "https://i.ibb.co/G76yssL/third.png",
+
+    }, {
+        id: 17,
+        name: 'Matériel Tbilisi',
+        newest: false,
+        name2: 'Nevy denim slim fit jeans',
+        price: 850,
+        img: "https://i.ibb.co/G76yssL/third.png",
+
+    }, {
+        id: 18,
+        name: 'Matériel Tbilisi',
+        newest: false,
+        name2: 'Nevy denim slim fit jeans',
+        price: 850,
+        img: "https://i.ibb.co/G76yssL/third.png",
+
+    }, {
+        id: 19,
+        name: 'Matériel Tbilisi',
+        newest: false,
+        name2: 'Nevy denim slim fit jeans',
+        price: 850,
+        img: "https://i.ibb.co/G76yssL/third.png",
+
+    }, {
+        id: 20,
+        name: 'Matériel Tbilisi',
+        newest: false,
+        name2: 'Nevy denim slim fit jeans',
+        price: 850,
+        img: "https://i.ibb.co/G76yssL/third.png",
+
+    }, {
+        id: 21,
+        name: 'Matériel Tbilisi',
+        newest: false,
+        name2: 'Nevy denim slim fit jeans',
+        price: 850,
+        img: "https://i.ibb.co/G76yssL/third.png",
+
+    }, {
+        id: 22,
+        name: 'Matériel Tbilisi',
+        newest: false,
+        name2: 'Nevy denim slim fit jeans',
+        price: 850,
+        img: "https://i.ibb.co/G76yssL/third.png",
+
+    }, {
+        id: 25,
+        name: 'Matériel Tbilisi',
+        newest: false,
+        name2: 'Nevy denim slim fit jeans',
+        price: 850,
+        img: "https://i.ibb.co/G76yssL/third.png",
+
+    }, {
+        id: 25,
+        name: 'Matériel Tbilisi',
+        newest: false,
+        name2: 'Nevy denim slim fit jeans',
+        price: 850,
+        img: "https://i.ibb.co/b5xW8yw/second.png"
+
+    }, {
+        id: 25,
+        name: 'Matériel Tbilisi',
+        newest: false,
+        name2: 'Nevy denim slim fit jeans',
+        price: 850,
+        img: "https://i.ibb.co/b5xW8yw/second.png"
+
+    }, {
+        id: 25,
+        name: 'Matériel Tbilisi',
+        newest: false,
+        name2: 'Nevy denim slim fit jeans',
+        price: 850,
+        img: "https://i.ibb.co/b5xW8yw/second.png"
+
+    }, {
+        id: 25,
+        name: 'Matériel Tbilisi',
+        newest: false,
+        name2: 'Nevy denim slim fit jeans',
+        price: 850,
+        img: "https://i.ibb.co/G76yssL/third.png",
+
+    }, {
+        id: 24,
+        name: 'Matériel Tbilisi',
+        newest: false,
+        name2: 'Nevy denim slim fit jeans',
+        price: 850,
+        img: "https://i.ibb.co/b5xW8yw/second.png"
+
+    }
+];
+
+
+
+///////////////////////////////////////////////////////////////
+
+
+function initial(arr) {
+    var mainContainer = document.getElementById("mainContainer");
+    mainContainer.innerHTML = ''
+    for (const item of arr) {
+        mainContainer.innerHTML += `<div class="product-item-1" id="${item.id}">
+        <img src="${item.img}" alt="first" value="image">
+        <p>${item.name}</p>
+        <p>${item.name2}</p>
+        <p>${item.price} usd</p>
+      </div>`
+    }
+}
+
+// price high
+function orderByPriceHighest(arr) {
+    var highestPrices = arr.sort(function(a, b) {
+        if (a.price < b.price) return 1
+        else return -1
+    })
+    return highestPrices
+}
+// price Low
+function orderByPriceLowest(arr) {
+    var lowPrices = arr.sort(function(a, b) {
+        if (a.price < b.price) return -1
+        else return 1
+    })
+    return lowPrices
+}
+
+function filterNewests(arr) {
+    return arr.filter(function(element) {
+        return element.newest == true;
+    })
+}
+
+function filterByPicks(arr) {
+    return arr.filter(function(element) {
+        return element.type === types.OUR_PICKS
+    })
+}
+
+function filterByDesign(arr) {
+    return arr.filter(function(element) {
+        return element.design === 'ingorokva';
+    })
+}
+
+function filterByPriceRange(arr, minPrice, maxPrice) {
+    return arr.filter(function(element) {
+        return element.price >= minPrice && element.price <= maxPrice
+    })
+}
+
+///////////// onclick
+document.getElementsByClassName("newest")[0].addEventListener('click', function(e) {
+    const newstProducts = filterNewests(arr)
+    initial(newstProducts)
+});
+
+document.getElementsByClassName('picks')[0].addEventListener('click', function(e) {
+    const ourpicks = filterByPicks(arr)
+    initial(ourpicks)
+});
+document.getElementsByClassName('Designers')[0].addEventListener('click', function(e) {
+    const designers = filterByDesign(arr)
+    initial(designers);
+});
+document.getElementsByClassName('price1')[0].addEventListener('click', function(e) {
+    const pricehigh = orderByPriceHighest(arr)
+    initial(pricehigh);
+})
+document.getElementsByClassName('price2')[0].addEventListener('click', function(e) {
+    const pricelow = orderByPriceLowest(arr)
+    initial(pricelow);
+})
+
+
+//init slider
+var slider = document.getElementById('slider');
+noUiSlider.create(slider, {
+    start: [0, 1000],
+    connect: true,
+    range: {
+        'min': 0,
+        'max': 1100,
+    },
+    tooltips: true,
+});
+
+const pricesMintoMax = orderByPriceLowest(arr)
+slider.noUiSlider.set([pricesMintoMax[0].price + 200, pricesMintoMax[pricesMintoMax.length - 1].price - 150]);
+
+// Binding signature
+slider.noUiSlider.on('change', function(e) {
+    const prices = slider.noUiSlider.get()
+    const filteredPrices = filterByPriceRange(arr, prices[0], prices[1])
+    initial(filteredPrices)
+});
+
+initial(arr)
+const test = orderByPriceLowest(arr)
